fbb72e1c68eeffea3ca442bfdee0349edf425515
AliensGoo <AliensGoo@users.noreply.github.com>
2014-02-07 23:56:12 +0000

    added_mac_pi2

--- testing/bootScript/change_host.py
+++ testing/bootScript/change_host.py
@@ -3,7 +3,7 @@ import random
 
 #Vars
 MyPI = "ChangeMe-ls"+str(random.randint(0, 99))
-MAC_Address = (("B8:27:EB:60:57:67", "Pi1"), ("B8:27:EB:34:8A:7A","Pi3"))
+MAC_Address = (("B8:27:EB:60:57:67", "Pi1"), ("B8:27:EB:34:8A:7A","Pi3"), ("B8:27:EB:20:C0:BA", "Pi2"))
 
 MyMAC = os.popen("ifconfig eth0 | grep HWaddr | awk '{print $5}'").read().lower()
 
@@ -12,5 +12,3 @@ for mac, pi in MAC_Address:
 		MyPI = pi
 
 os.popen("sudo echo "+pi+" > /etc/hostname")
-
-
