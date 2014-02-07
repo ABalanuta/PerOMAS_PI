4e346b2c929be40d4a989530898c64dd5443f5d7
AliensGoo <AliensGoo@users.noreply.github.com>
2014-02-07 22:43:36 +0000

    send mail at login

---
+++ testing/bootScript/change_host.py
@@ -0,0 +1,16 @@
+import os
+import random
+
+#Vars
+MyPI = "ChangeMe-ls"+str(random.randint(0, 99))
+MAC_Address = (("B8:27:EB:60:57:67", "Pi1"), ("B8:27:EB:34:8A:7A","Pi3"))
+
+MyMAC = os.popen("ifconfig eth0 | grep HWaddr | awk '{print $5}'").read().lower()
+
+for mac, pi in MAC_Address:
+	if mac.lower() is MyMAC:
+		MyPI = pi
+
+os.popen("sudo echo "+pi+" > /etc/hostname")
+
+
