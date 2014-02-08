50cf3e1e80600319d388c161ba13c3c3279f555c
AliensGoo <Artur.Balanuta@gmail.com>
2014-02-08 00:10:47 +0000

    fixes to get mac adress

--- testing/bootScript/change_host.py
+++ testing/bootScript/change_host.py
@@ -3,12 +3,14 @@ import random
 
 #Vars
 MyPI = "ChangeMe-ls"+str(random.randint(0, 99))
-MAC_Address = (("B8:27:EB:60:57:67", "Pi1"), ("B8:27:EB:34:8A:7A","Pi3"), ("B8:27:EB:20:C0:BA", "Pi2"))
+MAC_Address = (("b8:27:eb:60:57:67", "Pi1"), ("B8:27:EB:34:8A:7A","Pi3"), ("B8:27:EB:20:C0:BA", "Pi2"))
 
-MyMAC = os.popen("ifconfig eth0 | grep HWaddr | awk '{print $5}'").read().lower()
+MyMAC = os.popen("ifconfig eth0 | grep HWaddr | awk '{print $5}'").read().lower().split('\n')[0]
 
 for mac, pi in MAC_Address:
-	if mac.lower() is MyMAC:
+	if MyMAC in mac:
+		print "YOU ARE "+pi
 		MyPI = pi
+		break
 
 os.popen("sudo echo "+pi+" > /etc/hostname")
