3bfd6c83dc64f671e7a2bb0c952536ba0a2bf180
AliensGoo <AliensGoo@users.noreply.github.com>
2014-02-18 20:10:32 +0000

    web javascript graph

--- testing/modules/web/app/views.py
+++ testing/modules/web/app/views.py
@@ -1,6 +1,11 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, send_from_directory
 from app import app
 from forms import LoginForm
+import os
+
+@app.route('/favicon.ico')
+def favicon():
+	return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 @app.route('/index')
@@ -9,6 +14,14 @@ def index():
     return render_template("index.html",
         title = 'Home',
         user = user)
+        
+@app.route('/settings')
+def settings():
+	return render_template("settings.html")
+
+@app.route('/gateway')
+def gateway():
+	return render_template("gateway.html")
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -22,3 +35,4 @@ def login():
     return render_template('login.html', 
         title = 'Sign In',
         form = form)
+
