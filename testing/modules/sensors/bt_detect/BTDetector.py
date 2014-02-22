873b8dfc4798532cb70427c80f314798bd708f1c
AliensGoo <AliensGoo@users.noreply.github.com>
2014-02-22 21:27:31 +0000

    discovery balance

--- testing/modules/sensors/bt_detect/BTDetector.py
+++ testing/modules/sensors/bt_detect/BTDetector.py
@@ -9,6 +9,12 @@ from datetime import datetime
 
 
 class BTDetector(Thread):
+
+	stopped = None
+	hub = None
+	paired_devices = None
+	paired_status = None #For every time the device is seen its get incremented one, else subtracted
+	paired_status_maximum = 3
 	
 	def __init__(self, hub):
 		Thread.__init__(self)
@@ -36,7 +42,7 @@ class BTDetector(Thread):
 		
 		#Get Pared Devices
 		self.renew_paired_list()
-	
+
 	def register_phone(self, mac, pin):
 		
 		mac = mac.upper()
@@ -54,10 +60,10 @@ class BTDetector(Thread):
 			return "Pair OK"
 		else:
 			return "Pair Fail"
-			
+
 	def remove_phone(self, mac):
 		subprocess.Popen('sudo bluez-test-device remove '+mac, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-	
+
 	def renew_paired_list(self):
 		p = subprocess.Popen('sudo bluez-test-device list', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 		lines = p.stdout.readlines()
@@ -66,31 +72,74 @@ class BTDetector(Thread):
 			found_mac = line.split()[0]
 			new_list += [found_mac]
 		self.paired_devices = new_list
-	
+
 	def get_paired(self):
 		return self.paired_devices
-		
+
 	def stop(self):
 		self.stopped = True
 	
-	
+	#Checks if the device is on using l2ping
+	def l2ping(self, mac):
+		p = subprocess.Popen('sudo l2ping '+mac+' -s 0 -c 1', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		lines = p.stdout.readlines()
+		device_on = False
+		if len(lines) == 3 and mac in lines[1] and '1 received' in lines[2]:
+			device_on = True
+		return device_on
+		
 	def run(self):
-		#					mac
-		#sudo l2ping 40:B0:FA:3D:5F:08 -s 0 -c 1
-		print "Run not Implemented"
+		count = 0
+		while not self.stopped:
+			for device in self.paired_devices:
+				status = self.l2ping(device)
+				#self.update_device_status(device, status)
+				#todo uncoment
+				count += 1
+				#print count, device
+			sleep(1)
+			
+	def device_status(self, device):
+		print "Device Status not Implemented"
+			 
+	def update_device_status(self, device, status):
 
+		if not self.paired_status:
+			self.paired_status = []
+				
+		for x in self.paired_status:
+			print x
+			if device == x[0]:
+				old_status = x[1]
+				new_status = old_status + status
+				if not new_status > self.paired_status_maximum and not new_status < 0:
+					x[1] = new_status
+					return
+									
+		#if not inicialized creates new variable
+		print 'Not found in the list'
+		if status > 0 #TODO HERE
+		#TODO HERE
+		if not len(self.paired_status) > 0:
+			self.paired_status = [[device, status]]
+		else:
+			self.paired_status += [device, status]
+		print self.paired_status			
+			  
+		
 	def update(self):
 		print "Update not Implemented"
 
 #Runs only if called
 if __name__ == "__main__":
 
-    started = datetime.now()
+	started = datetime.now()
 
-    print "#TEST#"
-    print "#Starting#"
-    d = BTDetector(None)
-    print d.register_phone('40:B0:FA:3D:5F:08', '0000')
-    print d.get_paired()
-    
-    print "#Sttoped#\n\n"
+	print "#TEST#"
+	print "#Starting#"
+	d = BTDetector(None)
+	#print d.register_phone('40:B0:FA:3D:5F:08', '0000')
+	print d.get_paired()
+	#d.l2ping('40:B0:FA:3D:5F:08')
+	d.start()
+	print "#Sttoped#\n\n"
