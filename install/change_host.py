a5d98c2d6b98938aa11c57efe4d294c10a520e67
AliensGoo <AliensGoo@users.noreply.github.com>
2014-03-19 22:25:36 +0000

    change host script added macs

--- install/change_host.py
+++ install/change_host.py
@@ -3,17 +3,27 @@ import random
 
 #Vars
 MyPI = "ChangeMe-ls"+str(random.randint(0, 99))
-MAC_Address = (("b8:27:eb:60:57:67", "Pi1"), ("B8:27:EB:34:8A:7A","Pi3"), ("B8:27:EB:20:C0:BA", "Pi2"))
+
+MAC_Address = (
+	("b8:27:eb:60:57:67", "Pi1"),
+	("B8:27:EB:20:C0:BA", "Pi2"),
+	("B8:27:EB:34:8A:7A", "Pi3"),
+	("b8:27:eb:d1:19:df", "Pi4"),
+	("b8:27:eb:aa:e9:d2", "Pi5"),
+	("b8:27:eb:10:4a:74", "Pi6")
+	)
 
 MyMAC = os.popen("ifconfig eth0 | grep HWaddr | awk '{print $5}'").read().lower().split('\n')[0]
 
 for mac, pi in MAC_Address:
-	if MyMAC in mac:
-		print "YOU ARE "+pi
+	if MyMAC in mac.lower():
+		print "You are "+pi
 		MyPI = pi
 		break
 
-os.popen("sudo chmod 777 /etc/hosts")
-os.popen("sudo echo '127.0.1.1	'"+MyPI+" >> /etc/hosts")
-os.popen("sudo echo "+MyPI+" > /etc/hostname")
-os.popen("sudo chmod 644 /etc/hosts")
+#os.popen("sudo chmod 777 /etc/hosts")
+#os.popen("sudo echo '127.0.1.1	'"+MyPI+" >> /etc/hosts")
+#os.popen("sudo echo "+MyPI+" > /etc/hostname")
+#os.popen("sudo chmod 644 /etc/hosts")
+
+print "You Need to reboot the Pi"
