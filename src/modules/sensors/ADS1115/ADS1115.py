586141c8396c43f841bc2faea757055e77ecee62
AliensGoo <AliensGoo@users.noreply.github.com>
2014-04-21 21:10:44 +0100

    integration into the Main Class

---
+++ src/modules/sensors/ADS1115/ADS1115.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+"""Consumption Estimation
+
+Uses the ADS1x15 Adafruit lib to read values from the first
+channel of the ADC and converts to Real power consumption in Watts.
+"""
+
+__author__ = "Artur Balanuta"
+__version__ = "1.0.2"
+__email__ = "artur.balanuta [at] tecnico.ulisboa.pt"
+
+
+from time import sleep
+from threading import Thread
+from datetime import datetime
+from numpy import mean
+from Adafruit_ADS1x15 import ADS1x15
+
+
+
+class ADS1115(Thread):
+
+	UPDATE_INTERVAL 	= 0 	# time in seconds
+	ADS1115 			= 0x01	# 16-bit ADC
+	CHANNEL 			= 0 	# ADC Channel (from 0 to 3)
+	PGA					= 2048	# programable gain, possible values (256,512,1024,2048,4096,6144)
+	SAMPLES 			= 8 	# Samples per Second, possible values (8,16,32,128,256,475,860)
+	CALIBRATION_FACTOR 	= 2.4 	# Used to ajust the read values to the real consumption values
+	BASE_CONSUMPTION	= 1.0  	# Base confumption of the sensor
+	MESURMENTS			= 2		# Number of mesurments per second (Hz)
+
+
+	def __init__(self, hub):
+		Thread.__init__(self)
+		self.hub = hub
+		self.started = datetime.now()
+		self.last_update = datetime.now()
+		self.watts = 0.0
+		self.watts_memory_values = []
+		self.sensor = ADS1x15(ic=self.ADS1115)
+		self.sensor.startContinuousConversion(self.CHANNEL, self.PGA, self.SAMPLES) #reads values +-1V with 860 samples/s
+		self.stopped = False
+		#Runs after var declaration
+		self.update()# Runs one time
+
+
+	def stop(self):
+		self.stopped = True
+		sleep(1.1)
+		self.sensor.stopContinuousConversion()
+
+	def run(self):
+		self.stopped = False
+		while not self.stopped:
+			self.update()
+			if not self.stopped:
+				sleep(self.UPDATE_INTERVAL)
+
+	def update(self):
+
+		values = []
+		for x in range(0, self.MESURMENTS):
+			values.append(self.sensor.getLastConversionResults())
+			sleep(1/self.MESURMENTS)
+		self.watts = mean(values) * self.CALIBRATION_FACTOR - self.BASE_CONSUMPTION
+		self.watts_memory_values.append(self.watts)
+		#self.last_update = datetime.now()
+			
+	def runtime(self):
+		return str(self.last_update-self.started).split(".")[0]
+
+	#Retrurns the last read value
+	def getValue(self):
+		return self.watts
+
+	#Dumps the cache of luminosity values
+	def dumpMemoryValues(self):
+		w = self.watts_memory_values
+		self.watts_memory_values = []
+		return w
+		
+#Runs only if called
+if __name__ == "__main__":
+	
+	print "#TEST#"
+	print "#Starting#"
+	l = ADS1115(None)
+	l.start()
+	sleep(1)
+	try:
+		while True:
+			print l.watts
+			sleep(1)
+	except: 
+		l.stop()
+	print "#Sttoped#\n\n"
