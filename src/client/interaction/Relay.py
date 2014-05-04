06a67cd873e9dcb9dc4f4f49edb9733cb9eefc91
AliensGoo <AliensGoo@users.noreply.github.com>
2014-05-04 19:38:17 +0100

    releys reorganized

--- src/client/interaction/Relay.py
+++ src/client/interaction/Relay.py
@@ -15,14 +15,12 @@ class Relay():
 	RELAY_1_PINS	= [4, 17, 27, 22]	# GPIO.BCM Pin for the four chanels of the Relay
 	RELAY_2_PINS	= [23, 24, 25, 18]	#
 
-	RELAY 			= {	"LIGHTS_X1" 		:	{"Pin" : RELAY_1_PINS[0], "State" : True},
-						"LIGHTS_X2" 		:	{"Pin" : RELAY_1_PINS[1], "State" : False},
-						"POWER_SOCKETS_1"	:	{"Pin" : RELAY_1_PINS[2], "State" : False},
-						"POWER_SOCKETS_2"	:	{"Pin" : RELAY_1_PINS[3], "State" : False},
-						"AC_ON_OFF" 		:	{"Pin" : RELAY_2_PINS[0], "State" : False},
-						"AC_SPEED_1_2" 		:	{"Pin" : RELAY_2_PINS[1], "State" : False},
-						"AC_SPEED_3" 		:	{"Pin" : RELAY_2_PINS[2], "State" : False},
-						"AC_HEAT_COOL" 		:	{"Pin" : RELAY_2_PINS[3], "State" : False} }
+	RELAY 			= {	"AC_SPEED_1"		:	{"Pin" : 4, "State" : False},
+						"AC_SPEED_2"		:	{"Pin" : 17, "State" : False},
+						"AC_SPEED_3" 		:	{"Pin" : 27, "State" : False},
+						"AC_HEAT_COOL" 		:	{"Pin" : 22, "State" : False},
+						"LIGHTS_X1" 		:	{"Pin" : 23, "State" : False},
+						"LIGHTS_X2" 		:	{"Pin" : 24, "State" : False},}
 
 
 	def __init__(self, hub):
@@ -30,44 +28,39 @@ class Relay():
 		GPIO.setmode(GPIO.BCM)
 		GPIO.setwarnings(True)
 		
-		for x in self.RELAY_1_PINS:
-			GPIO.setup(x, GPIO.OUT)
-
-		for x in self.RELAY_2_PINS:
-			GPIO.setup(x, GPIO.OUT)
-
-		#Sets the Predefine state to each device
+		#Iniciates and Sets the Predefine state to each device
 		for device_name in self.RELAY.keys():
 			values = self.RELAY[device_name]
+			GPIO.setup(values["Pin"], GPIO.OUT)
 			GPIO.output(values["Pin"], values["State"])
 
-	def set_lights_x1_state(self, state):
-		if self.DEBUG:
-			print "Relay: set_lights_x1_state:", state
+	#def set_lights_x1_state(self, state):
+	#	if self.DEBUG:
+	#		print "Relay: set_lights_x1_state:", state
 
-		pin = self.RELAY["LIGHTS_X1"]["Pin"]
-		self.RELAY["LIGHTS_X1"]["State"] = state
-		GPIO.output(pin, state)
+	#	pin = self.RELAY["LIGHTS_X1"]["Pin"]
+	#	self.RELAY["LIGHTS_X1"]["State"] = state
+	#	GPIO.output(pin, state)
 
-	def set_lights_x2_state(self, state):
-		if self.DEBUG:
-			print "Relay: set_lights_x2_state:", state
+	#def set_lights_x2_state(self, state):
+	#	if self.DEBUG:
+	#		print "Relay: set_lights_x2_state:", state
 
-		pin = self.RELAY["LIGHTS_X2"]["Pin"]
-		self.RELAY["LIGHTS_X2"]["State"] = state
-		GPIO.output(pin, state)
+	#	pin = self.RELAY["LIGHTS_X2"]["Pin"]
+	#	self.RELAY["LIGHTS_X2"]["State"] = state
+	#	GPIO.output(pin, state)
 
-	def get_lights_x1_state(self):
-		if self.DEBUG:
-			print "Relay: get_lights_x1_state"
+	#def get_lights_x1_state(self):
+	#	if self.DEBUG:
+	#		print "Relay: get_lights_x1_state"
 
-		return self.RELAY["LIGHTS_X1"]["State"]
+	#	return self.RELAY["LIGHTS_X1"]["State"]
 
-	def get_lights_x2_state(self):
-		if self.DEBUG:
-			print "Relay: get_lights_x2_state"
+	#def get_lights_x2_state(self):
+	#	if self.DEBUG:
+	#		print "Relay: get_lights_x2_state"
 
-		return self.RELAY["LIGHTS_X2"]["State"]
+	#	return self.RELAY["LIGHTS_X2"]["State"]
 
 	def stop(self):
 		if self.DEBUG:
@@ -79,26 +72,8 @@ class Relay():
 if __name__ == "__main__":
 
 	r = Relay(None)
-
-
-	for x in r.RELAY_1_PINS:
-		GPIO.output(x, True)
-
-	for x in r.RELAY_2_PINS:
-		GPIO.output(x, True)
-
-	for x in r.RELAY_1_PINS:
-		GPIO.output(x, False)
-
-	for x in r.RELAY_2_PINS:
-		GPIO.output(x, False)
-
-	r.set_lights_x2_state(True)
-	#sleep(1)
-	r.set_lights_x1_state(True)
-	#sleep(1)
-	r.set_lights_x2_state(False)
-	#sleep(1)
-	r.set_lights_x2_state(True)
+	pin = r.RELAY["AC_SPEED_1"]["Pin"]
+	GPIO.output(pin, True)
+	sleep(5)
 	#sleep(1)
 	r.stop()
