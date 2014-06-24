bfafa5f551eac6900d66fcc411cbb789ece15679
AliensGoo <AliensGoo@users.noreply.github.com>
2014-06-24 21:30:15 +0100

    new Icons theme

--- src/client/interaction/Relay.py
+++ src/client/interaction/Relay.py
@@ -15,14 +15,14 @@ class Relay():
 						"AC_SPEED_2"		:	{"Pin" : 17, "State" : False},
 						"AC_SPEED_3" 		:	{"Pin" : 27, "State" : False},
 						"AC_HEAT_COOL" 		:	{"Pin" : 22, "State" : False},
-						"LIGHTS_X1" 		:	{"Pin" : 23, "State" : False},
-						"LIGHTS_X2" 		:	{"Pin" : 23, "State" : False},}
+						"LIGHTS_X1" 		:	{"Pin" : 14, "State" : True},
+						"LIGHTS_X2" 		:	{"Pin" : 15, "State" : True},}
 
 
 	def __init__(self, hub):
 		self.hub = hub
 		GPIO.setmode(GPIO.BCM)
-		GPIO.setwarnings(True)
+		GPIO.setwarnings(False)
 		
 		#Iniciates and Sets the Predefine state to each device
 		for device_name in self.RELAY.keys():
