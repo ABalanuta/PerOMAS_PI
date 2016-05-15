1ac31f92d118daf9f92a4d8337707a9bd70947d3
Artur Balanuta <artur.balanuta@gmail.com>
2016-05-15 23:14:58 +0100

    reconfigured the reboot and shutdown sequence

--- src/client/scripts/stop_peromas_and_shutdown.sh
+++ src/client/scripts/stop_peromas_and_shutdown.sh
@@ -1,8 +1,8 @@
 sudo tmux send -t main.0 C-c
-sleep 1
-sudo fbi -T 2 -d /dev/fb1 -noverbose -a /home/artur-adm/git/thesisPI/src/client/scripts/blue.jpg
-sleep 5
-sudo killall python
+#sleep 1
+#sudo fbi -T 2 -d /dev/fb1 -noverbose -a /home/artur-adm/git/thesisPI/src/client/scripts/blue.jpg
 sudo fbi -T 2 -d /dev/fb1 -noverbose -a /home/artur-adm/git/thesisPI/src/client/scripts/power.jpg
-sleep 1
-sudo halt&
+sleep 15
+sudo killall python
+sleep 3
+sudo halt
